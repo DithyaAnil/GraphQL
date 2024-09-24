60200785f849333d0444d0833d47c7414c717ff0
DithyaAnil <adithyaanil002@gmail.com>
2024-09-24 11:19:10 +0530

    Adding to GraphQL List and refetchQueries

--- src/components/AddOrder.tsx
+++ src/components/AddOrder.tsx
@@ -1,13 +1,56 @@
 import { useState } from "react";
+import { useMutation, gql } from "@apollo/client";
 
 export type AppProps = {
   customerId: number;
 };
 
+const GET_DATA = gql`
+  {
+    customers {
+      id
+      name
+      industry
+      orders {
+        id
+        description
+        totalInCents
+      }
+    }
+  }
+`;
+
+const MUTATE_DATA = gql`
+  mutation MUTATE_DATA(
+    $description: String!
+    $totalInCents: Int!
+    $customer: ID!
+  ) {
+    createOrder(
+      customer: $customer
+      description: $description
+      totalInCents: $totalInCents
+    ) {
+      order {
+        id
+        customer {
+          id
+        }
+        description
+        totalInCents
+      }
+    }
+  }
+`;
+
 export default function AddOrder({ customerId }: AppProps) {
   const [active, setActive] = useState(false);
-  const [cost, setCost] = useState(NaN);
+  const [total, setTotal] = useState(NaN);
   const [description, setDescription] = useState("");
+
+  const [createOrder, { loading, error, data }] = useMutation(MUTATE_DATA, {
+    refetchQueries: [{ query: GET_DATA }],
+  });
   return (
     <div>
       {active ? null : (
@@ -24,8 +67,13 @@ export default function AddOrder({ customerId }: AppProps) {
           <form
             onSubmit={(e) => {
               e.preventDefault();
-              /* createCustomer({
-                variables: { name: name, industry: industry },*/
+              createOrder({
+                variables: {
+                  customer: customerId,
+                  description: description,
+                  totalInCents: total * 100,
+                },
+              });
             }}
           >
             <div>
@@ -40,25 +88,26 @@ export default function AddOrder({ customerId }: AppProps) {
               />
             </div>
             <div>
-              <label htmlFor="cost">Cost:</label>
+              <label htmlFor="total">Total:</label>
               <input
-                id="cost"
+                id="total"
                 type="number"
-                value={isNaN(cost) ? "" : cost}
+                value={isNaN(total) ? "" : total}
                 onChange={(e) => {
-                  setCost(parseFloat(e.target.value));
+                  setTotal(parseFloat(e.target.value));
                 }}
               />
             </div>
             <br />
             {/* 
-            <button disabled={createCustomerLoading ? true : false}>
-              Add Customer
+            <button disabled={createOrderLoading ? true : false}>
+              Add Order
             </button>
-            {createCustomerError ? <p>Error creating customer</p> : null}
+            {createOrderError ? <p>Error creating order</p> : null}
             */}
-            <button>Add order</button>
+            <button disabled={loading ? true : false}>Add order</button>
           </form>
+          {error ? <p>Something wrong</p> : null}
         </div>
       ) : null}
     </div>
